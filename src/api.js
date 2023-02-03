1284f80d2cf2b0c76d1658bebd6c8bc2fb425b81
Selam Yihew <selamyihew@macbook-pro-2.gateway.sonic.net>
2023-02-03 10:37:54 -0800

    added search component, fetched data from weatherAPI

---
+++ src/api.js
@@ -0,0 +1,10 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'ee4a9927demshd2930473f69d32cp1cc681jsnfb1ddfa7fba0',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
